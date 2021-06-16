61fb884c9e6ecf4bed3aef384a3b99dc8c1d6480
Mindaugas Jasiunas <mindaugas.jasiunas.it@gmail.com>
2021-06-16 13:16:41 +0300

    Created checkout page. Closes #12

--- spring-mvc-webapp/src/main/java/com/example/controller/HomeController.java
+++ spring-mvc-webapp/src/main/java/com/example/controller/HomeController.java
@@ -3,6 +3,7 @@ package com.example.controller;
 import com.example.demo.dao.UserEntityRepository;
 import com.example.demo.entity.UserEntity;
 import com.example.demo.service.UserEntityService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -22,6 +23,11 @@ public class HomeController {
     @RequestMapping("/product")
     public String showProductPage() {
         return "product-page";
+    }
+
+    @RequestMapping("checkout")
+    public String showCheckoutPage(){
+        return "checkout";
     }
 
 
