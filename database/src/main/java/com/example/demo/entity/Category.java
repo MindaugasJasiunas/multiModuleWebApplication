bbbb57b787de0eedb76a0b94ddbb713c57c6d296
Mindaugas Jasiunas <mindaugas.jasiunas.it@gmail.com>
2021-06-17 15:51:24 +0300

    Updated Category entity #22

--- database/src/main/java/com/example/demo/entity/Category.java
+++ database/src/main/java/com/example/demo/entity/Category.java
@@ -2,6 +2,7 @@ package com.example.demo.entity;
 
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.*;
@@ -9,6 +10,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 @Data
+@EqualsAndHashCode
 
 @Entity
 public class Category {
@@ -16,10 +18,12 @@ public class Category {
     @SequenceGenerator( name = "images_sequence", sequenceName = "images_sequence", allocationSize = 1)
     @GeneratedValue( strategy = GenerationType.SEQUENCE, generator = "images_sequence")
     @Column(name = "id", unique = true)
+    @EqualsAndHashCode.Include
     private long id;
     @Column(name = "category", unique = true)
     private String categoryName;
 
+    @EqualsAndHashCode.Exclude
     @ManyToMany(mappedBy = "categories", fetch = FetchType.EAGER)
     private Set<Item> items=new HashSet<>();
 }
