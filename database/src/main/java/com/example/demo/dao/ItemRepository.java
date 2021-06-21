9a5bb9685f2f95ceafffd22011d34afcdcbcca70
Mindaugas Jasiunas <mindaugas.jasiunas.it@gmail.com>
2021-06-21 16:32:56 +0300

    Item Repository updated. #21

--- database/src/main/java/com/example/demo/dao/ItemRepository.java
+++ database/src/main/java/com/example/demo/dao/ItemRepository.java
@@ -1,9 +1,17 @@
 package com.example.demo.dao;
 
 import com.example.demo.entity.Item;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+import java.util.UUID;
+
 @Repository
 public interface ItemRepository extends PagingAndSortingRepository<Item, Long> {
+    Optional<Item> findItemByPublicId(UUID publicID);
 }
