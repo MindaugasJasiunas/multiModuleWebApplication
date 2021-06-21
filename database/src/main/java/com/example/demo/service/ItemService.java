d86abfd40cbf3012e51948309efef34743d92053
Mindaugas Jasiunas <mindaugas.jasiunas.it@gmail.com>
2021-06-21 16:27:30 +0300

    Item service created. Closes #33

--- database/src/main/java/com/example/demo/service/ItemService.java
+++ database/src/main/java/com/example/demo/service/ItemService.java
@@ -1,9 +1,20 @@
 package com.example.demo.service;
 
 import com.example.demo.entity.Item;
+import com.example.demo.entity.Store;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
+
 public interface ItemService {
     Page<Item> findAll(Pageable pageable);
+    int ItemQuantityInWarehouse(Item item);
+    Map<Store, Integer> getMapWithStoresAndQuantitiesForItem(Item item);
+    Optional<Item> findItemByPublicId(UUID publicId);
+    long pageCount(int itemsPerPage);
+    List<Item> getItemsForRelatedProducts(int howMany);
 }
--- database/src/main/java/com/example/demo/service/ItemServiceImpl.java
+++ database/src/main/java/com/example/demo/service/ItemServiceImpl.java
@@ -1,23 +1,84 @@
 package com.example.demo.service;
 
 import com.example.demo.dao.ItemRepository;
+import com.example.demo.dao.StoreItemRepository;
+import com.example.demo.dao.StoreRepository;
 import com.example.demo.entity.Item;
+import com.example.demo.entity.Store;
+import com.example.demo.entity.StoreItem;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.*;
 
 @Service
 public class ItemServiceImpl implements ItemService{
     private ItemRepository itemRepository;
+    private StoreRepository storeRepository;
+    private StoreItemRepository storeItemRepository;
 
-    public ItemServiceImpl(ItemRepository itemRepository) {
+    public ItemServiceImpl(ItemRepository itemRepository, StoreRepository storeRepository, StoreItemRepository storeItemRepository) {
         this.itemRepository = itemRepository;
+        this.storeRepository = storeRepository;
+        this.storeItemRepository = storeItemRepository;
     }
 
     @Override
     public Page<Item> findAll(Pageable pageable){
         return itemRepository.findAll(pageable);
+    }
+
+    @Override
+    public int ItemQuantityInWarehouse(Item item){
+        int leftInWarehouse=0;
+        if(storeRepository.getStoreByStoreTitle("Warehouse").isPresent()){
+            Store warehouse= storeRepository.getStoreByStoreTitle("Warehouse").get();
+            if(storeItemRepository.findStoreItemByItemAndStore(item, warehouse).isPresent()){
+                StoreItem warehouseStoreItem= storeItemRepository.findStoreItemByItemAndStore(item, warehouse).get();
+                leftInWarehouse=warehouseStoreItem.getQuantity();
+            }
+        }
+        return leftInWarehouse;
+    }
+
+    @Override
+    public Map<Store, Integer> getMapWithStoresAndQuantitiesForItem(Item item) {
+        Map<Store, Integer> storeWithQuantity=new HashMap<>();
+
+        Iterable<StoreItem> storeWithItemQuantity= storeItemRepository.findStoreItemsByItem_Id(item.getId());
+        storeWithItemQuantity.forEach(storeItem -> storeWithQuantity.put(storeItem.getStore(), storeItem.getQuantity()));
+        return storeWithQuantity;
+    }
+
+    @Override
+    public Optional<Item> findItemByPublicId(UUID publicId){
+        return itemRepository.findItemByPublicId(publicId);
+    }
+
+    @Override
+    public long pageCount(int itemsPerPage){
+        // return how many pages there is at all
+        long countElements= itemRepository.count();
+        long fullPages = countElements/itemsPerPage;
+        long notFullPages= countElements%itemsPerPage;
+        return notFullPages==0 ? fullPages : fullPages+1;
+    }
+
+    @Override
+    public List<Item> getItemsForRelatedProducts(int howMany) {
+//        Pageable pageAndSize= PageRequest.of(0, howMany);  //org.springframework.data.domain.Pageable
+//        return itemRepository.findAll(pageAndSize).getContent();
+        //randomize items:
+        List<Item> items= (List<Item>) itemRepository.findAll();
+        int count= (int) itemRepository.count();
+        List<Item> itemsRandomized=new ArrayList<>();
+        for(int i=0;i<howMany; i++){
+            itemsRandomized.add(items.get(new Random().nextInt(count)));
+        }
+        return itemsRandomized;
     }
 
 
