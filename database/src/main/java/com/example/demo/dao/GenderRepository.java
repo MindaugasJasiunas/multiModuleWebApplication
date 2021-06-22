11e8d347f74ee107c16300676b2060097a74cc9f
Mindaugas Jasiunas <mindaugas.jasiunas.it@gmail.com>
2021-06-22 13:55:28 +0300

    Updated Gender repository #28

--- database/src/main/java/com/example/demo/dao/GenderRepository.java
+++ database/src/main/java/com/example/demo/dao/GenderRepository.java
@@ -4,6 +4,9 @@ import com.example.demo.entity.Gender;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface GenderRepository extends CrudRepository<Gender, Long> {
+    Optional<Gender> findGenderByGender(String genderName);
 }
