cc1f9dec419cbb9d3dcc8791aad7c2e2ed00859e
Mindaugas Jasiunas <mindaugas.jasiunas.it@gmail.com>
2021-06-22 13:54:48 +0300

    Updated HomeController

--- spring-mvc-webapp/src/main/java/com/example/controller/HomeController.java
+++ spring-mvc-webapp/src/main/java/com/example/controller/HomeController.java
@@ -1,16 +1,10 @@
 package com.example.controller;
 
-import com.example.demo.dao.UserEntityRepository;
-import com.example.demo.entity.UserEntity;
 import com.example.demo.service.ItemService;
 import com.example.demo.service.ItemSortingService;
-import com.example.demo.service.UserEntityService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
