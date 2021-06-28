508fec0009ce18cf44d463b2b998489c1aa68e8f
Mindaugas Jasiunas <mindaugas.jasiunas.it@gmail.com>
2021-06-28 19:04:38 +0300

    Updated product page HTML, added new endpoint for adding item to cart in CartController. #54

--- spring-mvc-webapp/src/main/java/com/example/controller/CartController.java
+++ spring-mvc-webapp/src/main/java/com/example/controller/CartController.java
@@ -6,7 +6,12 @@ import com.example.demo.service.UserEntityService;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.UUID;
 
 @Controller
 public class CartController {
@@ -26,6 +31,12 @@ public class CartController {
         }
         //load items to shopping cart by user
         return "cart";
+    }
+
+    @PostMapping("/addToCart/{itemPublicId}")
+    public String processCheckout(@AuthenticationPrincipal UserEntity user, @PathVariable("itemPublicId") UUID itemPublicId, @RequestParam(value = "inputQuantity", defaultValue = "0") String quantity) {
+        //return redirect to the same item
+        return "redirect:/item/"+itemPublicId+"?added";
     }
 
 }
