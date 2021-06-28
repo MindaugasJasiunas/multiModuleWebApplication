9ad24520fc3fb973fde4d1273b5a4abd3db96559
Mindaugas Jasiunas <mindaugas.jasiunas.it@gmail.com>
2021-06-28 18:31:36 +0300

    Updated Item controller if item not found - throw 404, product-page changed - if 0 items left in warehouse, hide [Add To Cart] button

--- spring-mvc-webapp/src/main/java/com/example/controller/ItemController.java
+++ spring-mvc-webapp/src/main/java/com/example/controller/ItemController.java
@@ -8,11 +8,13 @@ import com.example.demo.entity.Store;
 import com.example.demo.entity.StoreItem;
 import com.example.demo.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.HashMap;
 import java.util.List;
@@ -30,15 +32,13 @@ public class ItemController {
 
     @GetMapping("{publicId}")
     public String getItem(@PathVariable("publicId") UUID publicId, Model model){
-        Item item= itemService.findItemByPublicId(publicId).orElseThrow(() -> new RuntimeException("Item with public id "+publicId.toString()+" doesn't exist."));
+        Item item= itemService.findItemByPublicId(publicId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Item Not Found"));
 
         Map<Store, Integer> map = itemService.getMapWithStoresAndQuantitiesForItem(item);
 
-
         model.addAttribute("relatedItems", itemService.getItemsForRelatedProducts(4));
-
         model.addAttribute("storeWithQuantityMap", map);
-        model.addAttribute("leftInWarehouse", itemService.ItemQuantityInWarehouse(item)); // if there is none - disable 'Add To Cart' button
+        model.addAttribute("leftInWarehouse", itemService.ItemQuantityInWarehouse(item));
         model.addAttribute("item", item);
         return "product-page";
     }
